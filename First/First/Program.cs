4d6f6f5bbb564db2b149bab829ba0ab22bdb7862
kurtzpb <kurtzpb@gmail.com>
2016-01-22 15:49:18 +0200

    Classes

--- First/First/Program.cs
+++ First/First/Program.cs
@@ -1,15 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace First
 {
+
+    class Rectangle
+    {
+        double side1;
+        double side2;
+        double area;
+        double perimeter;
+
+        public Rectangle(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+
+        public void PerimeterCalculator()
+        {
+            perimeter = (side1 * 2) + (side2 * 2);
+        }
+
+        public void AreaCalculator()
+        {
+            area = side1 * side2;
+        }
+
+        public double Area
+        {
+            get
+            {
+                return area;
+            }
+        }
+        public double Perimeter
+        {
+            get
+            {
+                return perimeter;
+            }
+        }
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Rectangle rec = new Rectangle(15, 43);
+            rec.AreaCalculator();
+            rec.PerimeterCalculator();
+
+            Console.WriteLine("Area = {0}; Perimeter = {1}",  rec.Area, rec.Perimeter);
+            Console.ReadKey();
         }
     }
 }
